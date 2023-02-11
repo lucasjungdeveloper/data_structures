042dfbd867cf7d8e07313bc6a56ff1b98973a6a5
Lucas Jung <lucasjungdeveloper@gmail.com>
2023-02-10 22:12:59 -0400

    feat: add node to beggining linked list

--- linked_list.c
+++ linked_list.c
@@ -6,9 +6,13 @@ typedef struct node {
     struct node *next;
 } node;
 
-void add_node_to_end(node** list, int number);
-void print_list(node* list);
-void free_list(node* list);
+void add_node_to_end(node **list, int number);
+
+void add_node_to_beginning(node **list, int number);
+
+void print_list(node *list);
+
+void free_list(node *list);
 
 int main(void) {
     node *list = NULL;
@@ -17,6 +21,8 @@ int main(void) {
     add_node_to_end(&list, 2);
     add_node_to_end(&list, 3);
     add_node_to_end(&list, 4);
+    add_node_to_beginning(&list, 0);
+    add_node_to_beginning(&list, -1);
 
     print_list(list);
 
@@ -24,9 +30,9 @@ int main(void) {
     return 0;
 }
 
-void add_node_to_end(node** list, int number) {
+void add_node_to_end(node **list, int number) {
     // Allocates memory for a node "n" and instantiate it
-    node* n = malloc(sizeof(node));
+    node *n = malloc(sizeof(node));
     n->number = number;
     n->next = NULL;
 
@@ -40,7 +46,7 @@ void add_node_to_end(node** list, int number) {
 
     // Copies "*list" pointed value to avoid changing
     // the list variable on the main function
-    node* node = *list;
+    node *node = *list;
 
     while (node->next != NULL) {
         node = node->next;
@@ -50,7 +56,15 @@ void add_node_to_end(node** list, int number) {
     node->next = n;
 }
 
-void print_list(node* list) {
+void add_node_to_beginning(node **list, int number) {
+    node *n = malloc(sizeof(node));
+    n->number = number;
+    n->next = *list;
+
+    *list = n;
+}
+
+void print_list(node *list) {
     while (list != NULL) {
         printf("%i\n", list->number);
         list = list->next;
@@ -60,7 +74,7 @@ void print_list(node* list) {
 void free_list(node *list) {
     while (list != NULL) {
         // Temporarily saves value of next node to safely free list's address
-        node* tmp = list->next;
+        node *tmp = list->next;
 
         free(list);
         list = tmp;
